63946997a4ef8332cecee497b30e1133350838fe
clusterr023 <44799769+clusterr023@users.noreply.github.com>
2018-11-06 10:51:49 +0100

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -968,7 +968,14 @@ public class InAppBrowser extends CordovaPlugin {
             String newloc = "";
             if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("file:")) {
                 newloc = url;
-            } else if (url.startsWith(WebView.SCHEME_TEL)) {
+            }else if(url.startsWith("pluspasapp:"){
+		    
+	    Intent intent = new Intent(Intent.ACTION_VIEW);
+                   intent.setData(Uri.parse(url));
+                   cordova.getActivity().startActivity(intent);
+                   return true;
+		    
+	    }else if (url.startsWith(WebView.SCHEME_TEL)) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_DIAL);
                     intent.setData(Uri.parse(url));
